4acab84eaf2999b773c50bb9afc0f0c4603e77f7
ubuntu <ubuntu@ubuntu-virtual-machine.(none)>
2012-06-02 15:23:56 -0500

    test

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pilsencallao_cheers',          # Or path to database file if using sqlite3.
+        'NAME': 'tesis_db',          # Or path to database file if using sqlite3.
         'USER': '',                         # Not used with sqlite3.
         'PASSWORD': '',                   # Not used with sqlite3.
         'HOST': '',     # Set to empty string for localhost. Not used with sqlite3.
